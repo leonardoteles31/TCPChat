957cee50c332dbc8cb042ffc99575532dbf2e182
Leonardo Teles <leo.teleso.10@hotmail.com>
2026-07-13 21:55:37 -0300

    documenting code

--- backend/server.py
+++ backend/server.py
@@ -12,43 +12,72 @@ server.listen()
 clients = []
 nicknames = []
 
-
+# Sends a message to all connected clients.
 def broadcast(message):
     for client in clients:
         client.send(message)
 
 
+# Receives messages from the client and broadcasts them to all other connected users.
 def handle(client):
     while True:
         try:
+            # Receive data from the client (maximum 1024 bytes)
             message = client.recv(1024)
+
+            # Send received message to all connected clients
             broadcast(message)
         except:
+            # Client disconnected, remove it from the server
             index = clients.index(client)
             clients.remove(client)
             client.close()
+
+            # Get the nickname of the disconnected client
             nickname = nicknames[index]
+
+            # Notify remaining users
             broadcast(f'{nickname} left the chat!'.encode('ascii'))
+
+            # Remove nickname from the list
             nicknames.remove(nickname)
             break
 
 
 def receive():
+
+    """
+    Accepts new client connections and initializes them.
+
+    Creates a new thread for each client so multiple users
+    can communicate simultaneously.
+    """
+
     while True:
+        # Accept a new client connection
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
+        # Ask the client for their nickname
         client.send('NICK'.encode('ascii'))
+
+        # Receive and store the nick name
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
         print(f'Nickname of the client is {nickname}!')
+
+        # Notify everyone that a new user joined
         broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+
+        # Confirm connection to the new client
         client.send('Connected to the server!'.encode('ascii'))
 
+        # Create a new thread to handle this client independently
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
 
 print("Server is online!")
 receive()
